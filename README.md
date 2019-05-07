47aed2b3d56600b81237078f3d067997f449b1aa
bryanhfoster <bryanhfoster@gmail.com>
2019-05-07 12:36:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-7fehgm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-7fehgm)
---
+++ src/app/_nav.ts
@@ -0,0 +1,21 @@
+export const navItems = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    name: 'Dashboard',
+    url: '/schedule',
+    icon: 'icon-calendar'
+  },
+  {
+    name: 'Test',
+    url: '/test',
+    icon: 'icon-coffee'
+  }
+];
